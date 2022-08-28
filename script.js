727c386907b392c6958f286e683c1bff117f6afa
Alena <elenila_skinef@mail.ru>
2022-08-28 20:58:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
+Hello friend! Thank you for visiting the carnival!`);
+console.log(`Here's the list of gifts:\n`);
+console.log(`Teddy Bear
+Big Red Ball
+Huge Bear
+Candy
+Stuffed Tiger
+Stuffed Dragon
+Skateboard
+Toy Car
+Basketball
+Scary Mask`);
