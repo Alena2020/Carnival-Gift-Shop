5046aab7ddf884dadf03902a8b007917e5c0a1bc
Alena <elenila_skinef@mail.ru>
2022-08-29 18:02:33 +0300

    user selects action

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+let input = require('sync-input');
+let tickets = 100;
+
+class Gift {
+    constructor(name, price, id) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
+
+    buy() {
+      console.log(`Here you go, one ${this.name}!`);
+      tickets -= this.price; 
+    }
+}
+
 let gifts = [
-  {name: 'Teddy Bear', price: '10', id: '1'},
-  {name: 'Big Red Ball', price: '5', id: '2'},
-  {name: 'Huge Bear', price: '50', id: '3'},
-  {name: 'Candy', price: '8', id: '4'},
-  {name: 'Stuffed Tiger', price: '15', id: '5'},
-  {name: 'Stuffed Dragon', price: '30', id: '6'},
-  {name: 'Skateboard', price: '100', id: '7'},
-  {name: 'Toy Car', price: '25', id: '8'},
-  {name: 'Basketball', price: '20', id: '9'},
-  {name: 'Scary Mask', price: '75', id: '10'}
+  new Gift("Teddy Bear", 10, 1),
+  new Gift("Big Red Ball", 5, 2),
+  new Gift("Huge Bear", 50, 3),
+  new Gift("Candy", 8, 4),
+  new Gift("Stuffed Tiger", 15, 5),
+  new Gift("Stuffed Dragon", 30, 6),
+  new Gift("Skateboard", 100, 7),
+  new Gift("Toy Car", 25, 8),
+  new Gift("Basketball", 20, 9),
+  new Gift("Scary Mask", 75, 10),
 ];
 
 function printWelcoming() {
@@ -16,12 +32,62 @@ function printWelcoming() {
 Hello friend! Thank you for visiting the carnival!`);
 }
 
-function printGifts() {
+function buyGift() {
+  let id = Number(input(`Enter the number of the gift you want to get:`));
+  let gift = gifts.find(el => el.id === id);
+  gift.buy();
+  checkTickets();
+}
+
+function addTickets() {
+  tickets += Number(input(`Enter the ticket amount:`));
+  checkTickets();  
+}
+
+function checkTickets() {
+  console.log(`Total tickets: ${tickets}`);
+}
+
+function showGifts() {
   console.log(`Here's the list of gifts:\n`);
   gifts.forEach( gift => {
     console.log(`${gift.id}- ${gift.name}, Cost: ${gift.price} tickets`)
   });
 }
 
+function printMessage() {
+  console.log(`Have a nice day!`);
+}
+
+function selectAction() {
+  let action = Number(input(`\nWhat do you want to do?
+1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts`));
+
+  switch (action) {
+    case 1:
+      buyGift();        
+      break;
+
+    case 2:
+      addTickets();       
+      break;
+
+    case 3:
+      checkTickets();      
+      break;
+
+    case 4:
+      showGifts();   
+      break;      
+  
+    default:
+      console.log(`Wrong input`);
+      break;
+  }
+
+  printMessage();
+}
+
 printWelcoming();
-printGifts();
+showGifts();
+selectAction();
